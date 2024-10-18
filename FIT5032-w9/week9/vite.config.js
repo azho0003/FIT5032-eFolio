a9d673fab59a2abee7b92ad2de318e668636f424
Alex Zhou <azho0003@student.monash.edu>
2024-10-18 20:28:59 +1100

    Github pages deployment

--- FIT5032-w9/week9/vite.config.js
+++ FIT5032-w9/week9/vite.config.js
@@ -4,6 +4,12 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
+module.exports = {
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/<repository-name>/'
+    : '/'
+}
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
